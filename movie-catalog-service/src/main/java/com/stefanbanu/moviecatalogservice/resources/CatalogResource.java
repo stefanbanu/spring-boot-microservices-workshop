15c45a95b7d89b07f3d8595ddeccbdf3aa90e3bc
stefanbanu <stefanbanu1@gmail.com>
2021-09-06 16:14:10 +0300

    added hystrix library, circuit breaker.

--- movie-catalog-service/src/main/java/com/stefanbanu/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/stefanbanu/moviecatalogservice/resources/CatalogResource.java
@@ -1,5 +1,7 @@
 package com.stefanbanu.moviecatalogservice.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.stefanbanu.moviecatalogservice.models.Movie;
 import com.stefanbanu.moviecatalogservice.models.UserRating;
 import com.stefanbanu.moviecatalogservice.models.CatalogItem;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +27,13 @@ public class CatalogResource {
     WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "500")
+            })
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
@@ -35,6 +45,10 @@ public class CatalogResource {
                 })
                 .collect(Collectors.toList());
 
+    }
+
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+        return Arrays.asList(new CatalogItem("No movies", "", 0));
     }
 }
 
